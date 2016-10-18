16bfda840ad091e376bbeb756752898d3dc17c40
Shaun Fuller <fullers@email.unc.edu>
2016-10-17 22:42:36 -0400

    updated code

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,9 @@
 
+var totalDifference = 0;
+
 module.exports = function(app, friends) {
 
-
+// API GET Request - Allows user to view the 
 app.get('/api/:friends?', function (req, res) {
 	
 	var chosen = req.params.friends;
@@ -26,15 +28,45 @@ app.get('/api/:friends?', function (req, res) {
 
 
 // Create New Characters - takes in JSON input
-app.post('/api/friends', function (req, res) {
-	var newfriend = req.body;
-	//newfriend.routeName = newfriend.name.replace(/\s+/g, '').toLowerCase();
+app.post('/api/friends', function(req, res){
 
-	console.log(newfriend);
+		var bestMatch = {
+			name: "none",
+			image: "none",
+		};
 
-	friends.push(newfriend);
+		var usrData 	= req.body;
+		var usrName 	= usrData.name;
+		var usrImage 	= usrData.image;
+		var usrScores 	= usrData.scores;
 
-	res.json(newfriend);
-});
+		var totalDifference = 0;
 
-}
+		//loop through the friends data array of objects to get each friends scores
+		for(var i = 0; i < [friends].length-1; i++){
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			//loop through that friends score and the users score and calculate the 
+			// absolute difference between the two and push that to the total difference variable set above
+			for(var j = 0; j < 10; j++){
+				// We calculate the difference between the scores and sum them into the totalDifference
+				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
+				// If the sum of differences is less then the differences of the current "best match"
+				if (totalDifference <= bestMatch.friendDifference){
+
+					// Reset the bestMatch to be the new friend. 
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.matchDifference = totalDifference;
+				}
+			}
+		}
+
+		friends.push(usrData);
+ 
+		res.json(bestMatch);
+
+	});
+
+};
